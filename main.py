8c8bc929d10d35fc8454860ef3dd4f0ebe28a88e
NadHQ <d.kovalev2002@yandex.ru>
2022-11-09 14:31:37 +0300

    fix some bugs

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def on_shutdown(dispatcher):
 
 
 
-executor.start_webhook(skip_updates=True,
+executor.start_webhook(dispatcher=dp,skip_updates=True,
 webhook_path=WEBHOOK_PATH,
 on_startup=on_startup,
 on_shutdown=on_shutdown,
