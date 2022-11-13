215fd65a723f007dcd7941ae977f3283c48dc579
NadHQ <d.kovalev2002@yandex.ru>
2022-11-13 15:40:19 +0300

    fix

--- handlers/other.py
+++ handlers/other.py
@@ -9,10 +9,10 @@ async def inviteApplyMessage(chat_member: types.ChatJoinRequest):
         await bot.send_photo(chat_id= chat_member.from_user.id,photo=open("inputfile.txt","r").read() , caption=open("inputtext.txt","r").read())
     except BotBlocked:
         asyncio.sleep(0.1)
-        print('Bot Blocked:'+chat_member.from_user.id)
+        print('Bot Blocked:'+str(chat_member.from_user.id))
     except CantInitiateConversation:
         asyncio.sleep(0.1)
-        print('Cant Initiate Dialog:'+chat_member.from_user.id)
+        print('Cant Initiate Dialog:'+str(chat_member.from_user.id))
 
 def reg_handlers_other(dp : Dispatcher):
     dp.register_chat_join_request_handler(inviteApplyMessage)
