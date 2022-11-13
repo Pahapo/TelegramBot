fdbf16ee31ccccf2a4394c5079d520eb78ae62eb
NadHQ <d.kovalev2002@yandex.ru>
2022-11-13 14:49:43 +0300

    add exceptions

--- handlers/other.py
+++ handlers/other.py
@@ -2,13 +2,17 @@ from create_bot import dp, bot
 from aiogram import Dispatcher, types
 from aiogram import types
 import asyncio
+from aiogram.utils.exceptions import BotBlocked, CantInitiateConversation
 # @dp.chat_join_request_handler()
 async def inviteApplyMessage(chat_member: types.ChatJoinRequest):
     try:
         await bot.send_photo(chat_id= chat_member.from_user.id,photo=open("inputfile.txt","r").read() , caption=open("inputtext.txt","r").read())
-    except:
+    except BotBlocked:
         asyncio.sleep(0.1)
-        print(chat_member.from_user.id)
+        print('Bot Blocked:'+chat_member.from_user.id)
+    except CantInitiateConversation:
+        asyncio.sleep(0.1)
+        print('Cant Initiate Dialog:'+chat_member.from_user.id)
 
 def reg_handlers_other(dp : Dispatcher):
     dp.register_chat_join_request_handler(inviteApplyMessage)
