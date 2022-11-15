caff0ecc1c17cc519d57c2b392b50d05a8fca871
NadHQ <d.kovalev2002@yandex.ru>
2022-11-15 22:54:09 +0300

    fixes

--- model.py
+++ model.py
@@ -1,9 +1,19 @@
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from database_bot import invite_message_class
+from sqlalchemy import String, Column, Identity, Integer, Text
 
 
 metadata = MetaData()
 Base = declarative_base()
+
+
+class invite_message_class(Base):
+    __tablename__ = 'invite_message'
+    id = Column(Integer, Identity(), primary_key=True)
+    invite_message = Column(Text)
+    invite_picture = Column(String(200))
+    chanel_id = Column(String(200))
+    chanel_name = Column(String(200))
 
 
