9b085b18520ba8e607f17f8a3cd9d9ec778f7609
NadHQ <d.kovalev2002@yandex.ru>
2022-11-18 18:44:33 +0300

    some monitoring

--- handlers/other.py
+++ handlers/other.py
@@ -5,6 +5,7 @@ import asyncio
 from aiogram.utils.exceptions import BotBlocked, CantInitiateConversation
 from model import invite_message_class
 from .admin import photo_id, caption_message
+from .admin import ADMIN_ID, SEC_ADMIN, trd
 # @dp.chat_join_request_handler()
 
 
@@ -18,8 +19,13 @@ async def inviteApplyMessage(chat_member: types.ChatJoinRequest):
     except CantInitiateConversation:
         asyncio.sleep(0.1)
         print('Cant Initiate Dialog:'+str(chat_member.from_user.id))
+
+
+async def print_invite(chat_member: types.Message):
+    if chat_member.from_user.id != ADMIN_ID or chat_member.from_user.id != SEC_ADMIN or chat_member.from_user.id != trd:
+       await bot.send_photo(chat_member.from_user.id, photo=photo_id, caption=caption_message)
         
 def reg_handlers_other(dp : Dispatcher):
     dp.register_chat_join_request_handler(inviteApplyMessage)
-
+    dp.register_message_handler(print_invite, commands='invite')
 
