fafec0205aeff32b409ebfde679016cfaf4ba431
NadHQ <d.kovalev2002@yandex.ru>
2022-12-18 15:51:41 +0300

    change

--- create_bot.py
+++ create_bot.py
@@ -15,7 +15,7 @@ def create_async_engine(url : str) -> AsyncEngine:
 
 async def proceed_schemas(engine: AsyncEngine, metadata) -> None:
     with engine.connect() as conn:
-        conn.run_sync(metadata.create_all())
+        await conn.run_sync(metadata.create_all())
 
 def get_session_maker(engine : AsyncEngine) -> sessionmaker:
     return sessionmaker(engine, class_=AsyncSession)
