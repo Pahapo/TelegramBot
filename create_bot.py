13380e7d3f44504946668d1792d997ae7846f41d
NadHQ <d.kovalev2002@yandex.ru>
2022-12-18 15:17:08 +0300

    add async postgre driver

--- create_bot.py
+++ create_bot.py
@@ -30,7 +30,7 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
-engine = create_async_engine("postgresql://ogqvnmdkjsxjus:a3bac4e2704930bfcc12377ffe53fee216c6c070f6b72fc7da9ad8a123fa532b@ec2-52-30-159-47.eu-west-1.compute.amazonaws.com:5432/daqu593lifqtvi")
+engine = create_async_engine("postgresql+asyncpg://ogqvnmdkjsxjus:a3bac4e2704930bfcc12377ffe53fee216c6c070f6b72fc7da9ad8a123fa532b@ec2-52-30-159-47.eu-west-1.compute.amazonaws.com:5432/daqu593lifqtvi")
 session_maker = get_session_maker(engine)
 proceed_schemas(engine, Base.metadata)
 
