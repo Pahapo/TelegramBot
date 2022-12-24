04c1542d906fcbda043e7a60eaf168791e9db596
NadHQ <d.kovalev2002@yandex.ru>
2022-12-25 01:27:46 +0300

    fix

--- handlers/admin.py
+++ handlers/admin.py
@@ -98,7 +98,7 @@ async def load_invite_message(message: types.Message, state=FSMContext):
         global caption
         caption_message = data['invite']
         print(caption_message)
-        await write_info(data)
+        ioloop.run_until_complete(write_info(data))
         print(caption_message)
         await bot.send_photo(chat_id=message.from_user.id, photo=data['photo'], caption=data['invite'])
         await state.finish()
