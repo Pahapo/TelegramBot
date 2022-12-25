e61b84d2c86051134d83c1b751342e339148d1ed
NadHQ <d.kovalev2002@yandex.ru>
2022-12-25 12:04:55 +0300

    fix

--- handlers/admin.py
+++ handlers/admin.py
@@ -44,7 +44,7 @@ async def get_invite_message():
         photo_id = result.invite_picture
         print('123123123')
         await session.commit()
-        await session.close()
+        print(session.get_transaction())
 
 
 ioloop = asyncio.get_event_loop()
